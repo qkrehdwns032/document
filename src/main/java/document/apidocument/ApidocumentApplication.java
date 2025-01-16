b4f81b9535aff49a7912566490d99caf1c5f6a1e
DongJun Park <skhu202014128@gmail.com>
2025-01-16 16:47:13 +0900

    commit

---
+++ src/main/java/document/apidocument/ApidocumentApplication.java
@@ -0,0 +1,13 @@
+package document.apidocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApidocumentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/document/apidocument/ApidocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package document.apidocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidocumentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
