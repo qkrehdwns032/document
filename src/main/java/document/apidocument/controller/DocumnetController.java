b0fa8e7f94c9aaf0169049a3decd3cbb2c86f604
DongJun Park <skhu202014128@gmail.com>
2025-01-20 22:50:56 +0900

    documentController

---
+++ src/main/java/document/apidocument/controller/DocumnetController.java
@@ -0,0 +1,19 @@
+package document.apidocument.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DocumnetController {
+
+    // 글 작성
+
+    // 글 수정
+
+    // 삭제기능은 없이할까
+
+    // 문서에 user collaborator 초대 기능
+
+    // collaborator 초대를 받을 수 있는 부분 생각
+
+
+}
